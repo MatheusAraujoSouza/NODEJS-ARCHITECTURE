00373a9d6b3d3f625858194a61da31caa994b898
Matheus Araujo Souza <mhatheussss@gmail.com>
2023-05-23 23:38:51 -0300

    fix errors in connections with my table

--- TAKS/TASK-1/WEBPAGE/routes/views/pages/js/table.js
+++ TAKS/TASK-1/WEBPAGE/routes/views/pages/js/table.js
@@ -1,6 +1,8 @@
 fetch('/api/users')
   .then(response => response.json())
   .then(data => {
+    console.log('Fetched user data:', data); // Log the fetched data
+
     const tableBody = document.querySelector('#data-table tbody');
 
     data.forEach(item => {
@@ -9,27 +11,40 @@ fetch('/api/users')
         <td>${item.id}</td>
         <td>${item.name}</td>
         <td>${item.email}</td>
+        <td>
+          <button class="view-button">View</button>
+          <button class="edit-button">Edit</button>
+          <button class="delete-button">Delete</button>
+        </td>
       `;
-
-      // Add buttons for CRUD operations
-      const crudButtons = document.createElement('td');
-      crudButtons.innerHTML = `
-        <button onclick="getUser(${item.id})">View</button>
-        <button onclick="editUser(${item.id})">Edit</button>
-        <button onclick="deleteUser(${item.id})">Delete</button>
-      `;
-      row.appendChild(crudButtons);
 
       tableBody.appendChild(row);
+    });
+
+    // Add event listener to the table for button clicks
+    tableBody.addEventListener('click', event => {
+      const target = event.target;
+      const row = target.closest('tr');
+      const id = row.querySelector('td:first-child').innerText;
+
+      if (target.classList.contains('view-button')) {
+        getUser(id);
+      } else if (target.classList.contains('edit-button')) {
+        editUser(id);
+      } else if (target.classList.contains('delete-button')) {
+        deleteUser(id);
+      }
     });
   });
 
 function getUser(id) {
+  console.log('getUser called with id:', id); // Log the user id
+
   fetch(`/api/users/${id}`, { method: 'GET' })
     .then(response => response.json())
     .then(user => {
+      console.log('Fetched user:', user); // Log the fetched user
       // Display user details or perform other actions
-      console.log(user);
     })
     .catch(error => {
       console.error('Error fetching user:', error);
@@ -37,16 +52,13 @@ function getUser(id) {
 }
 
 function editUser(id) {
+  console.log('editUser called with id:', id); // Log the user id
+
   fetch(`/api/users/${id}`)
     .then(response => response.json())
     .then(user => {
+      console.log('Fetched user for editing:', user); // Log the fetched user
       // Implement logic to edit the user directly in the table
-      const nameElement = document.querySelector(`#data-table tr[data-id="${id}"] td.name`);
-      const emailElement = document.querySelector(`#data-table tr[data-id="${id}"] td.email`);
-
-      // For example, you can enable editing by making the fields editable
-      nameElement.contentEditable = true;
-      emailElement.contentEditable = true;
     })
     .catch(error => {
       console.error('Error fetching user:', error);
@@ -54,25 +66,23 @@ function editUser(id) {
 }
 
 function deleteUser(id) {
+  console.log('deleteUser called with id:', id);
+
   if (confirm('Are you sure you want to delete this user?')) {
     fetch(`/api/users/${id}`, { method: 'DELETE' })
       .then(response => response.json())
       .then(result => {
+        console.log('User deletion result:', result);
         // Handle successful deletion
-        console.log(result);
-        // Remove the deleted row from the table
-        const row = document.querySelector(`#data-table tr[data-id="${id}"]`);
-        if (row) {
-          row.remove();
-        }
       })
       .catch(error => {
         console.error('Error deleting user:', error);
       });
   }
 }
-
 function createUser(user) {
+  console.log('createUser called with user:', user); // Log the user object
+
   fetch('/api/users', {
     method: 'POST',
     headers: {
@@ -82,29 +92,14 @@ function createUser(user) {
   })
     .then(response => response.json())
     .then(data => {
+      console.log('Created user:', data); // Log the created user
       // Handle successful creation
-      console.log(data);
-
-      // Create a new row for the created user and add it to the table
-      const tableBody = document.querySelector('#data-table tbody');
-      const row = document.createElement('tr');
-      row.setAttribute('data-id', data.id);
-      row.innerHTML = `
-        <td>${data.id}</td>
-        <td>${data.name}</td>
-        <td>${data.email}</td>
-        <td>
-          <button onclick="getUser(${data.id})">View</button>
-          <button onclick="editUser(${data.id})">Edit</button>
-          <button onclick="deleteUser(${data.id})">Delete</button>
-        </td>
-      `;
-      tableBody.appendChild(row);
     })
     .catch(error => {
       console.error('Error creating user:', error);
     });
 }
+
 // Add event listener to the form's submit event
 document.getElementById('create-user-form').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the default form submission behavior
@@ -115,9 +110,11 @@ document.getElementById('create-user-form').addEventListener('submit', function(
 
   // Create a user object with the input values
   const user = {
-    name: "Bruna",
-    email: "Bruna@example.com"
+    name: name,
+    email: email
   };
+
+  console.log('Form submitted with user:', user); // Log the submitted user object
 
   // Call the createUser function with the user object
   createUser(user);
